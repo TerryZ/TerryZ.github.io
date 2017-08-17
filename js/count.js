036b81fd7b90841c705b3781bf9126f972a1fabd
TerryZ <terry5@foxmail.com>
2017-08-17 15:20:32 +0800

    update

---
+++ js/count.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2c195274cd3511a4924527cd6dd883a2";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
