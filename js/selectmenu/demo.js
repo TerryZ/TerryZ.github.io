59d0d19f63032f6be0fe17e14f2db756e7494c8f
Terry Zeng <terry5@foxmail.com>
2017-11-27 01:26:35 +0800

    update

--- js/selectmenu/demo.js
+++ js/selectmenu/demo.js
@@ -1,36 +1,36 @@
 $(function(){
-    var selectMenuData = [
-        {id:1 ,name:'Chicago Bulls',desc:'芝加哥公牛',abbr:'CHI'},
-        {id:2 ,name:'Cleveland Cavaliers',desc:'克里夫兰骑士',abbr:'CLE'},
-        {id:3 ,name:'Detroit Pistons',desc:'底特律活塞',abbr:'DET'},
-        {id:4 ,name:'Indiana Pacers',desc:'印第安纳步行者',abbr:'IND'},
-        {id:5 ,name:'Milwaukee Bucks',desc:'密尔沃基雄鹿',abbr:'MIL'},
-        {id:6 ,name:'Brooklyn Nets',desc:'布鲁克林篮网',abbr:'BKN'},
-        {id:7 ,name:'Boston Celtics',desc:'波士顿凯尔特人',abbr:'BOS'},
-        {id:8 ,name:'New York Knicks',desc:'纽约尼克斯',abbr:'NYK'},
-        {id:9 ,name:'Philadelphia 76ers',desc:'费城76人',abbr:'PHI'},
-        {id:10,name:'Toronto Raptors',desc:'多伦多猛龙',abbr:'TOR'},
-        {id:11,name:'Atlanta Hawks',desc:'亚特兰大老鹰',abbr:'ATL'},
-        {id:12,name:'Charlotte Hornets',desc:'夏洛特黄蜂',abbr:'CHA'},
-        {id:13,name:'Miami Heat',desc:'迈阿密热火',abbr:'MIA'},
-        {id:14,name:'Orlando Magic',desc:'奥兰多魔术',abbr:'ORL'},
-        {id:15,name:'Washington Wizards',desc:'华盛顿奇才',abbr:'WAS'},
-        {id:16,name:'Denver Nuggets',desc:'丹佛掘金',abbr:'DEN'},
-        {id:17,name:'Minnesota Timberwolves',desc:'明尼苏达森林狼',abbr:'MIN'},
-        {id:18,name:'Oklahoma City Thunder',desc:'俄克拉荷马雷霆',abbr:'OKC'},
-        {id:19,name:'Portland Trail Blazers',desc:'波特兰开拓者',abbr:'POR'},
-        {id:20,name:'Utah Jazz',desc:'犹他爵士',abbr:'UTA'},
-        {id:21,name:'Golden State Warriors',desc:'金州勇士',abbr:'GSW'},
-        {id:22,name:'Los Angeles Clippers',desc:'洛杉矶快船',abbr:'LAC'},
-        {id:23,name:'Los Angeles Lakers',desc:'洛杉矶湖人',abbr:'LAL'},
-        {id:24,name:'Phoenix Suns',desc:'菲尼克斯太阳',abbr:'PHX'},
-        {id:25,name:'Sacramento Kings',desc:'萨克拉门托国王',abbr:'SAC'},
-        {id:26,name:'Dallas Mavericks',desc:'达拉斯小牛',abbr:'DAL'},
-        {id:27,name:'Houston Rockets',desc:'休斯顿火箭',abbr:'HOU'},
-        {id:28,name:'Memphis Grizzlies',desc:'孟菲斯灰熊',abbr:'MEM'},
-        {id:29,name:'New Orleans Pelicans',desc:'新奥尔良鹈鹕',abbr:'NOP'},
-        {id:30,name:'San Antonio Spurs',desc:'圣安东尼奥马刺',abbr:'SAS'}
-    ];
+	var selectMenuData = [
+		{id:1 ,name:'Chicago Bulls',desc:'芝加哥公牛',abbr:'CHI'},
+		{id:2 ,name:'Cleveland Cavaliers',desc:'克里夫兰骑士',abbr:'CLE'},
+		{id:3 ,name:'Detroit Pistons',desc:'底特律活塞',abbr:'DET'},
+		{id:4 ,name:'Indiana Pacers',desc:'印第安纳步行者',abbr:'IND'},
+		{id:5 ,name:'Milwaukee Bucks',desc:'密尔沃基雄鹿',abbr:'MIL'},
+		{id:6 ,name:'Brooklyn Nets',desc:'布鲁克林篮网',abbr:'BKN'},
+		{id:7 ,name:'Boston Celtics',desc:'波士顿凯尔特人',abbr:'BOS'},
+		{id:8 ,name:'New York Knicks',desc:'纽约尼克斯',abbr:'NYK'},
+		{id:9 ,name:'Philadelphia 76ers',desc:'费城76人',abbr:'PHI'},
+		{id:10,name:'Toronto Raptors',desc:'多伦多猛龙',abbr:'TOR'},
+		{id:11,name:'Atlanta Hawks',desc:'亚特兰大老鹰',abbr:'ATL'},
+		{id:12,name:'Charlotte Hornets',desc:'夏洛特黄蜂',abbr:'CHA'},
+		{id:13,name:'Miami Heat',desc:'迈阿密热火',abbr:'MIA'},
+		{id:14,name:'Orlando Magic',desc:'奥兰多魔术',abbr:'ORL'},
+		{id:15,name:'Washington Wizards',desc:'华盛顿奇才',abbr:'WAS'},
+		{id:16,name:'Denver Nuggets',desc:'丹佛掘金',abbr:'DEN'},
+		{id:17,name:'Minnesota Timberwolves',desc:'明尼苏达森林狼',abbr:'MIN'},
+		{id:18,name:'Oklahoma City Thunder',desc:'俄克拉荷马雷霆',abbr:'OKC'},
+		{id:19,name:'Portland Trail Blazers',desc:'波特兰开拓者',abbr:'POR'},
+		{id:20,name:'Utah Jazz',desc:'犹他爵士',abbr:'UTA'},
+		{id:21,name:'Golden State Warriors',desc:'金州勇士',abbr:'GSW'},
+		{id:22,name:'Los Angeles Clippers',desc:'洛杉矶快船',abbr:'LAC'},
+		{id:23,name:'Los Angeles Lakers',desc:'洛杉矶湖人',abbr:'LAL'},
+		{id:24,name:'Phoenix Suns',desc:'菲尼克斯太阳',abbr:'PHX'},
+		{id:25,name:'Sacramento Kings',desc:'萨克拉门托国王',abbr:'SAC'},
+		{id:26,name:'Dallas Mavericks',desc:'达拉斯小牛',abbr:'DAL'},
+		{id:27,name:'Houston Rockets',desc:'休斯顿火箭',abbr:'HOU'},
+		{id:28,name:'Memphis Grizzlies',desc:'孟菲斯灰熊',abbr:'MEM'},
+		{id:29,name:'New Orleans Pelicans',desc:'新奥尔良鹈鹕',abbr:'NOP'},
+		{id:30,name:'San Antonio Spurs',desc:'圣安东尼奥马刺',abbr:'SAS'}
+	];
     var selectMenuGroupData = [{
         title : 'East',
         list :[
@@ -70,7 +70,7 @@ $(function(){
             {id:30,name:'San Antonio Spurs',desc:'圣安东尼奥马刺',abbr:'SAS'}
         ]}
     ];
-    var baseMenuData = [
+	var baseMenuData = [
         {content:'163 NetEase',url : 'http://www.163.com'},
         {content:'Sina',url : 'http://www.sina.com'},
         {content:'sm_divider'},
@@ -82,6 +82,31 @@ $(function(){
         {content:'Click this menu item to trigger your callback',callback : function(){
             bDialog.alert('you can do anything in callback!');
         }}
+    ];
+	var baseMultiLevelMenuData = [
+        {content:'Sports',menus: [
+            {content:'Fivb',url : 'http://www.fivb.com/'},
+            {content:'Fifa',url : 'http://www.fifa.com/'},
+            {content:'NBA',menus: [
+                {content:'NBA official site',url : 'http://www.nba.com'},
+                {content:'Chicago Bulls',url : 'http://www.nba.com/bulls/'},
+                {content:'Los Angeles Lakers',url : 'www.nba.com/lakers/'}
+            ]}
+        ]},
+        {content:'News',menus: [
+            {content:'BBC',url : 'http://www.bbc.com/news'},
+            {content:'CNN',url : 'http://www.cnn.com'},
+            {content:'Xinhua',url : 'http://www.xinhuanet.com'}
+        ]},
+        {content:'Technology',menus: [
+            {content:'Github',url : 'https://github.com'},
+            {content:'StackOverflow',url : 'https://stackoverflow.com/'},
+            {content:'Reddit',url : 'https://www.reddit.com'}
+        ]},
+        {content:'Social',menus: [
+            {content:'Facebook',url : 'https://www.facebook.com'},
+            {content:'Twitter',url : 'https://twitter.com'}
+        ]}
     ];
     var baseMenuHeaderData = [
         {content:'News Site',header: true},
@@ -99,10 +124,11 @@ $(function(){
             bDialog.alert('you can do anything in callback!');
         }}
     ];
-    $('#selectMenuBase').click(function(){
+	$('#selectMenuBase').click(function(){
         $(this).selectMenu({
             showField : 'name',
             keyField : 'id',
+            searchField : 'abbr',
             search : true,
             data : selectMenuData
         });
@@ -125,16 +151,16 @@ $(function(){
             data : selectMenuData
         });
     });
-    $('#selectMenuGroup').click(function(){
+	$('#selectMenuGroup').click(function(){
         $(this).selectMenu({
             showField : 'name',
             keyField : 'id',
-            listSize : 10,
+            listSize : 7,
             multiple : true,
             data : selectMenuGroupData
         });
     });
-    $('#selectMenuInit').click(function(){
+	$('#selectMenuInit').click(function(){
         $(this).selectMenu({
             showField : 'name',
             keyField : 'id',
@@ -161,6 +187,30 @@ $(function(){
             data : selectMenuData
         });
     });
+
+    $('#selectMenuCallback').click(function(){
+        var $box = $('#panelCallbackDemo');
+        $(this).selectMenu({
+            showField : 'name',
+            keyField : 'id',
+            multiple : true,
+            data : selectMenuGroupData,
+            eSelect : function(data){
+                $box.empty();
+                if(data && data.length){
+                    $.each(data, function(i, row){
+                        $box.append('<p>Selected '+row.name+' ( '+row.desc+' )</p>');
+                    });
+                }
+            },
+            eTabSwitch: function(index, tab){
+                $box.empty();
+                $box.append('<p>You switch to the Tab with index <strong>'+index+'</strong>, Tab name is <strong>'+tab.title+'</strong></p>');
+            }
+        });
+    });
+
+
     $('#selectMenuRightClickBase').selectMenu({
         title : 'Regular menu',
         regular : true,
@@ -173,6 +223,14 @@ $(function(){
         keyField : 'id',
         rightClick : true,
         data : selectMenuData
+    });
+    $('#selectMenuMoveOpen').hover(function(){
+        $(this).selectMenu({
+            regular : true,
+            data : baseMenuHeaderData
+        });
+    },function(){
+        $(this).selectMenuHide();
     });
     $('#btnAddRow').click(function(){
         var li = $('<li>').addClass('list-group-item').html('new menu item ' + ($('#ulDynamicMenu li').size()+1));
@@ -198,7 +256,7 @@ $(function(){
 
 
 
-    $('#baseMenu').click(function(){
+	$('#baseMenu').click(function(){
         $(this).selectMenu({
             regular : true,
             arrow : false,
@@ -240,6 +298,12 @@ $(function(){
             title : 'SelectMenu ',
             regular : true,
             data : baseMenuHeaderData
+        });
+    });
+    $('#baseMenuMultiLevel').click(function(){
+        $(this).selectMenu({
+            regular : true,
+            data : baseMultiLevelMenuData
         });
     });
 
