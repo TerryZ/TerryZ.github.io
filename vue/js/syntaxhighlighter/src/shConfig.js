5956c21397020868b3fb6b6acd13db81e5accda9
TerryZ <terry5@foxmail.com>
2019-11-03 15:12:55 +0800

    update

--- vue/js/syntaxhighlighter/src/shConfig.js
+++
@@ -1,4 +0,0 @@
-window.SyntaxHighlighter.defaults['auto-links'] = false
-window.SyntaxHighlighter.defaults.gutter = false
-window.SyntaxHighlighter.defaults['tab-size'] = 2
-window.SyntaxHighlighter.defaults.toolbar = false
