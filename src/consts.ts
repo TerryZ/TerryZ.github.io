f392cb30781bb9969a05cb7e64573e93638ebf07
TerryZeng <terry5@foxmail.com>
2025-12-24 23:53:27 +0800

    feat: 布局与结构

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Terry Devs';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
---
+++ src/contents/profile.md
@@ -0,0 +1,16 @@
+# Terry
+
+A lost coder
+
+## Location
+
+Fuzhou, China
+
+## Emails
+
+- terry5@foxmail.com
+- terryzeng5@gmail.com
+
+## Github
+
+[github.com/terryz](https://github.com/terryz)
